e59fdf4ab122b01be8af0661134222dad1673a09
刘一凡 <36919957+fangfucdwin@users.noreply.github.com>
2018-06-05 13:03:20 +0800

    Update html_downloader.py

--- html_downloader.py
+++ html_downloader.py
@@ -9,9 +9,6 @@ class HtmlDownloader(object):
     def download(self, url):
         if url is None:
             return None
-
-        #url_ = quote(url, safe=string.printable)
-        #response = request.urlopen(url_)
         kv = {
             'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
             'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.18 Safari/537.36',
@@ -21,11 +18,7 @@ class HtmlDownloader(object):
 
         response = requests.get(url, headers=kv)
         response.encoding = response.apparent_encoding
-
-        #if response.getcode() != 200:
-        #    return None
         if response.status_code != 200:
             return None
-
-        #return response.read()
+        
         return response.text
